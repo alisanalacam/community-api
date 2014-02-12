f147ba2874faed4cf063af83aca316ea37706847
Osman Ungur <osmanungur@gmail.com>
2014-02-12 19:41:44 +0200

    Implemented event controller actions and related repository methods

--- src/Phpist/Bundle/EventBundle/Controller/EventController.php
+++ src/Phpist/Bundle/EventBundle/Controller/EventController.php
@@ -3,18 +3,35 @@
 namespace Phpist\Bundle\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class EventController extends Controller
 {
 
-    public function getAction()
+    public function getAction($id)
     {
+        $event = $this->getDoctrine()->getRepository('PhpistEventBundle:Event')->findOneWithDetails($id);
 
+        if (empty($event)) {
+            throw $this->createNotFoundException('Event not found');
+        }
+
+        return new JsonResponse(
+            $event
+        );
     }
 
     public function getAllAction()
     {
+        $events = $this->getDoctrine()->getRepository('PhpistEventBundle:Event')->findAllWithDetails();
 
+        if (empty($events)) {
+            throw $this->createNotFoundException('Event list not found');
+        }
+
+        return new JsonResponse(
+            $events
+        );
     }
 
 }
--- src/Phpist/Bundle/EventBundle/Repository/EventRepository.php
+++ src/Phpist/Bundle/EventBundle/Repository/EventRepository.php
@@ -9,8 +9,36 @@ namespace Phpist\Bundle\EventBundle\Repository;
 
 
 use Doctrine\ORM\EntityRepository;
+use Phpist\Bundle\EventBundle\Entity\Event;
 
 class EventRepository extends EntityRepository
 {
+
+    public function findOneWithDetails($id)
+    {
+        return $this->getEntityManager()->createQuery(
+            'SELECT e, se, sp FROM PhpistEventBundle:Event e
+            LEFT JOIN e.sessions se
+            LEFT JOIN se.speaker sp
+            WHERE e.id = :id AND e.status = :status')
+            ->setParameters(
+                array('id' => $id, 'status' => Event::STATUS_ACTIVE)
+            )
+            ->getArrayResult();
+    }
+
+    public function findAllWithDetails()
+    {
+        return $this->getEntityManager()->createQuery(
+            'SELECT e, se, sp FROM PhpistEventBundle:Event e
+            LEFT JOIN e.sessions se
+            LEFT JOIN se.speaker sp
+            WHERE e.status = :status')
+            ->setParameters(
+                array('status' => Event::STATUS_ACTIVE)
+            )
+            ->getArrayResult();
+    }
+
 
 } 
