f6028e9feabc9bcc48b475ef15acd08181c898f7
mustafaileri <mstfleri@gmail.com>
2014-02-14 17:29:58 +0200

    Fixes #15. Repositories was moved to services and calling as a service, Integration tests was written for all actions in event bundle.

--- src/Phpist/Bundle/EventBundle/Controller/EventController.php
+++ src/Phpist/Bundle/EventBundle/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace Phpist\Bundle\EventBundle\Controller;
 
 use Doctrine\ORM\NoResultException;
+use Phpist\Bundle\EventBundle\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -11,18 +12,25 @@ class EventController extends Controller
 
     public function getAction($id)
     {
+        /** @var EventRepository $eventRepository */
+        $eventRepository = $this->container->get('phpist_event.repository.event_repository');
+
         try {
             return new JsonResponse(
-                $this->getDoctrine()->getRepository('PhpistEventBundle:Event')->findOneWithDetails($id)
+                $eventRepository->findOneWithDetails($id)
             );
         } catch (NoResultException $e) {
             throw $this->createNotFoundException('Event not found');
         }
+
     }
 
     public function getAllAction()
     {
-        $events = $this->getDoctrine()->getRepository('PhpistEventBundle:Event')->findAllWithDetails();
+        /** @var EventRepository $eventRepository */
+        $eventRepository = $this->container->get('phpist_event.repository.event_repository');
+
+        $events = $eventRepository->findAllWithDetails();
 
         if (empty($events)) {
             throw $this->createNotFoundException('Event list not found');
